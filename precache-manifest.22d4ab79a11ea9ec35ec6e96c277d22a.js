9a9bee23c97cae7767474e2fb9c2424648af07c3
Taivanjargal11 <s18ud1101039@nmit.edu.mn>
2021-10-31 16:24:51 +0800

    Updates

---
+++ precache-manifest.22d4ab79a11ea9ec35ec6e96c277d22a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280dd511fb4ba9a4b49df06e6b981bf8",
+    "url": "/brgr/index.html"
+  },
+  {
+    "revision": "bc19d83c93268a117856",
+    "url": "/brgr/static/css/main.8e0d4f36.chunk.css"
+  },
+  {
+    "revision": "3c6622280fc384580f14",
+    "url": "/brgr/static/js/2.e37e4841.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/brgr/static/js/2.e37e4841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc19d83c93268a117856",
+    "url": "/brgr/static/js/main.d764519c.chunk.js"
+  },
+  {
+    "revision": "bc6eba6bee0f324a745c",
+    "url": "/brgr/static/js/runtime-main.90f5b7bf.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/brgr/static/media/burger-logo.b8503d26.png"
+  }
+]);
